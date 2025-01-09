f0c6f871b6eff243ea921c705d955599be5cc4f6
lswteen <lswteen@gmail.com>
2025-01-09 20:14:56 +0900

    push

--- travel-api/target/generated-sources/annotations/com/lottecard/travel/app/hotel/mapper/HotelWebMapperImpl.java
+++ travel-api/target/generated-sources/annotations/com/lottecard/travel/app/hotel/mapper/HotelWebMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-01-09T20:14:15+0900",
+    date = "2025-01-09T20:14:46+0900",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.41.0.v20241217-1506, environment: Java 17.0.13 (Eclipse Adoptium)"
 )
 @Component
